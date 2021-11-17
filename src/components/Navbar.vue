7ace20c8df6a846d153dcf504cc634f7fdebcd4e
oussama reghaye <oussamareghaye1998@hotmail.com>
2021-11-17 11:46:37 +0100

    app bar with drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,34 @@
 <template>
     <nav>
-        <v-toolbar  flat app>
-            <v-toolbar-title>
+        <v-app-bar  flat app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text--uppercase grey--text">
                 <span class="font-weight-light">Simple</span>
                 <span>Todo</span>
-            </v-toolbar-title class="white--text">
+            </v-toolbar-title >
             <v-spacer></v-spacer>
-            <v-btn flat color="grey" app>
+            <v-btn flat  elevation="0" class="grey--text" app>
                 <span>
                     Sign Out
                 </span>
                 <v-icon right>exit_to_app</v-icon>
-
             </v-btn>
-        </v-toolbar>
+
+
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p class="white--text"> test</p>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            drawer:false
+        }
+    }
 }
 </script>
 
